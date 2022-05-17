52c4aa3da68cdd0872c3138f5903b2b384f29d1c
Макс <0w1.go.0w1@gmail.com>
2022-05-17 16:28:23 +0300

    Изменил создание моковых данных offers и сделал их отрисовку

--- src/mock/offer.js
+++ src/mock/offer.js
@@ -1,15 +1,15 @@
 import {getRandomInteger, createId} from '../utils.js';
-import {TYPES, DESCRIPTIONS} from '../const.js';
+import {TYPES, DESCRIPTIONS, OFFERS} from '../const.js';
 
 const offerId = new createId();
 
 const generateOffer = () => ({
     id: offerId.add(),
-    title: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
+    title: OFFERS[getRandomInteger(0, OFFERS.length - 1)],
     price: getRandomInteger(5, 200)
 });
 
-const offers = Array.from({length: getRandomInteger(0, 3)}, generateOffer);
+const offers = Array.from({length: getRandomInteger(0, OFFERS.length)}, generateOffer);
 
 const generateOffersType = () => ({
     offerType: TYPES[getRandomInteger(0, TYPES.length - 1)],
--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -36,9 +36,10 @@ const editHeaderPointTemplate = (point, offer) => (
 
     <div class="event__field-group  event__field-group--destination">
       <label class="event__label  event__type-output" for="event-destination-1">
-        Flight
+        ${point.type}
       </label>
-      <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Chamonix" list="destination-list-1">
+      <input class="event__input  event__input--destination" id="event-destination-1" type="text" 
+      name="event-destination" value="${point.destination.name}" list="destination-list-1">
       <datalist id="destination-list-1">
         ${destinationListTemplate()}
       </datalist>
@@ -68,9 +69,21 @@ const editHeaderPointTemplate = (point, offer) => (
   </header>`
 );
 
+const offersListTemplate = (offer) => offer.offers.map((item) => (
+  `<div class="event__offer-selector">
+    <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" 
+    name="event-offer-luggage">
+    <label class="event__offer-label" for="event-offer-luggage-1">
+      <span class="event__offer-title">${item.title}</span>
+      &plus;&euro;&nbsp;
+      <span class="event__offer-price">${item.price}</span>
+    </label>
+  </div>`
+));
+
 const editPointTemplate = (point, offer) => {
   const {price, destination, type, dateFrom, dateTo} = point;
-  const {offerType} = offer;
+  const {offerType, offers} = offer;
 
   const headerTemplate = editHeaderPointTemplate(point, offer);
   
@@ -82,50 +95,7 @@ const editPointTemplate = (point, offer) => {
           <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
           <div class="event__available-offers">
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
-              <label class="event__offer-label" for="event-offer-luggage-1">
-                <span class="event__offer-title">Add luggage</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">50</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
-              <label class="event__offer-label" for="event-offer-comfort-1">
-                <span class="event__offer-title">Switch to comfort</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">80</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-              <label class="event__offer-label" for="event-offer-meal-1">
-                <span class="event__offer-title">Add meal</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">15</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-              <label class="event__offer-label" for="event-offer-seats-1">
-                <span class="event__offer-title">Choose seats</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">5</span>
-              </label>
-            </div>
-
-            <div class="event__offer-selector">
-              <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-              <label class="event__offer-label" for="event-offer-train-1">
-                <span class="event__offer-title">Travel by train</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">40</span>
-              </label>
-            </div>
+            ${offersListTemplate(offer)}
           </div>
         </section>
 
