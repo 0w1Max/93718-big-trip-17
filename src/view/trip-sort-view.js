108a6667ce4d4b05b2a5f1430ebeaeaaffe46d85
Макс <0w1.go.0w1@gmail.com>
2022-06-07 14:58:12 +0300

    Добавил подписку на изменение сортировки

--- src/view/trip-sort-view.js
+++ src/view/trip-sort-view.js
@@ -35,4 +35,18 @@ export default class TripSortView extends AbstractView {
   get template () {
     return tripSortTemplate();
   }
+
+  setSortTypeChangeHandler = (callback) => {
+    this._callback.sortTypeChange = callback;
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
+  };
+
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.tagName !== 'INPUT') {
+      return;
+    }
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  };
 }
